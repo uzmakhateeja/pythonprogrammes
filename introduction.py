47a27608e99265bea8062cde26adefe28a8dfcc7
uzmakhateeja <uzmakhateeja@gmail.com>
2025-12-28 16:58:00 +0530

    Add files via upload

---
+++ introduction.py
@@ -0,0 +1,11 @@
+# Printing a greeting message
+print("Hello, world!")
+
+# Printing my name
+print("My name is Sufiyan.")
+
+# Printing a fun fact about myself
+print("I love playing games.")
+
+# Performing and printing a simple calculation
+print("The result of 3 + 5 is:", 3 + 5)
